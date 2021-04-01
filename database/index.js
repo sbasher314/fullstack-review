d4907eea91fda7cf342724e22df5a022a462f133
Steven Basher <sbasher314@gmail.com>
2021-03-31 22:44:33 -0400

    Complete db save function

--- database/index.js
+++ database/index.js
@@ -15,10 +15,27 @@ let repoSchema = new mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repos) => {
+  let promises = [];
+  for (let repo of repos) {
+    let newRepo = {
+      repoId: repo.id,
+      name: repo.name,
+      url: repo.html_url,
+      username: repo.owner.login,
+      userUrl: repo.owner.html_url,
+      forks: repo.forks,
+      watchers: repo.watchers,
+      createdAt: repo.created_at,
+      updatedAt: repo.updated_at
+    };
+    promises.push(
+      new Promise(resolve => {
+        Repo.findOneAndUpdate({repoId: repo.id}, newRepo, {upsert: true, new: true}, (err, doc) => resolve(doc));
+      })
+    );
+  }
+  return Promise.all(promises);
 }
 
 module.exports.save = save;
