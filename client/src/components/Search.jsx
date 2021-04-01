fd01e4d8e9b7b3088f4cee37ac289dd76e297e33
Steven Basher <sbasher314@gmail.com>
2021-03-31 21:03:56 -0400

    Fixed search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -14,16 +14,17 @@ class Search extends React.Component {
     });
   }
 
-  search() {
+  search(e) {
+    e.preventDefault();
     this.props.onSearch(this.state.term);
   }
 
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
-      <button onClick={this.search}> Add Repos </button>
-    </div>) 
+      Enter a github username: <input value={this.state.terms} onChange={(e) => this.onChange(e)}/>
+      <button onClick={(e) => this.search(e)}> Add Repos </button>
+    </div>)
   }
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,15 +7,18 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
 
   }
 
   search (term) {
-    console.log(`${term} was searched`);
-    // TODO
+    if (term.length > 0) {
+      console.log(`${term} was searched`);
+    } else {
+      console.log(`You didn't search anything`);
+    }
   }
 
   render () {
